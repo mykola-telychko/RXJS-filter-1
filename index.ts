424a0d0c4c50a4a08ac0e22ca5e1e6fb1867c407
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-22 00:32:58 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import { from } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/filtering/filter
+
+//Example 1: filter for even numbers
+
 //emit (1,2,3,4,5)
 const srcList$ = from([1, 2, 3, 4, 5, 22, 30, 31, 59]);
 //filter out non-even numbers
